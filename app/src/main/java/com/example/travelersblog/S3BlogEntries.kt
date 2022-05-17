81d8051e0b2d02b04a8126256891d0aeffb1dd4d
UnderCoverTable <sameed.gilani@gmail.com>
2022-05-17 11:55:47 +0500

    first-commit

---
+++ app/src/main/java/com/example/travelersblog/S3BlogEntries.kt
@@ -0,0 +1,11 @@
+package com.example.travelersblog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class S3BlogEntries : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_s3_blog_entries)
+    }
+}
