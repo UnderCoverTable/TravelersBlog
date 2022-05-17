d8c18ca74e1e48687ba689ac6330a00ecf4856f6
UnderCoverTable <sameed.gilani@gmail.com>
2022-05-17 13:15:24 +0500

    latest

--- app/src/main/java/com/example/travelersblog/S4NewEntry.kt
+++ app/src/main/java/com/example/travelersblog/S4NewEntry.kt
@@ -3,10 +3,9 @@ package com.example.travelersblog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class S3BlogEntries : AppCompatActivity() {
+class S4NewEntry : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_s3_blog_entries)
-
+        setContentView(R.layout.activity_s4_new_entry)
     }
 }
