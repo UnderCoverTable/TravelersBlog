750a64f78dd8339d20ade59c6cd0c8ec7ea56758
UnderCoverTable <sameed.gilani@gmail.com>
2022-05-22 01:02:04 +0500

    what -

--- app/src/main/java/com/example/travelersblog/S3BlogEntries.kt
+++ app/src/main/java/com/example/travelersblog/S3BlogEntries.kt
@@ -3,6 +3,10 @@ package com.example.travelersblog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.travelersblog.S3RecyclerView.adapter.ItemAdapter_s3
+import com.example.travelersblog.S3RecyclerView.data.DataSource_s3
+import com.example.travelersblog.S5RecyclerView.adapter.ItemAdapter
+import com.example.travelersblog.S5RecyclerView.data.Datasource
 import com.example.travelersblog.databinding.ActivityS3BlogEntriesBinding
 
 class S3BlogEntries : AppCompatActivity() {
@@ -14,13 +18,16 @@ class S3BlogEntries : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        val myDataset = DataSource_s3().loadInfo()
+        binding.s3RecyclerView.adapter = ItemAdapter_s3(myDataset)
+
         val guest: Boolean = intent.getBooleanExtra("guest", true)
 
-        if (guest) {
-            TODO("Show guest view with Logout & New button disabled")
-        }else {
-            TODO("Normal view for logged in User")
-        }
+        //if (guest) {
+       //     TODO("Show guest view with Logout & New button disabled")
+        //}else {
+        //    TODO("Normal view for logged in User")
+       // }
 
     }
 }
--- app/src/main/java/com/example/travelersblog/S3RecyclerView/model/Info.kt
+++ app/src/main/java/com/example/travelersblog/S3RecyclerView/model/Info.kt
@@ -1,12 +1,11 @@
 package com.example.travelersblog.S3RecyclerView.model
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 
 data class Info (
     @DrawableRes val imageResourceId: Int,
     val place: String,
-    val longDesc: String,
     val shortDesc: String,
-    val author: String
+    val author: String,
+    val longDesc: String
 )
