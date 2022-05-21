616d4bee0888c8dead70175ad5a16755e5f1b2ea
UnderCoverTable <sameed.gilani@gmail.com>
2022-05-21 22:31:28 +0500

    what

--- app/src/main/java/com/example/travelersblog/S3BlogEntries.kt
+++ app/src/main/java/com/example/travelersblog/S3BlogEntries.kt
@@ -1,16 +1,15 @@
 package com.example.travelersblog
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.example.travelersblog.databinding.ActivityS1LoginScreenBinding
+import com.example.travelersblog.databinding.ActivityS3BlogEntriesBinding
 
-class S3BlogEntries: AppCompatActivity() {
-    private lateinit var binding: ActivityS1LoginScreenBinding
+class S3BlogEntries : AppCompatActivity() {
+    private lateinit var binding: ActivityS3BlogEntriesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityS1LoginScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        supportActionBar?.hide()
-    }
+        binding = ActivityS3BlogEntriesBinding.inflate(layoutInflater)
+        setContentView(binding.root)    }
 }
